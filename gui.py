412dab1a64786d88391ae0e6f1d6887c22d5a7ab
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-03 16:37:20 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 
 
 class Gui:
@@ -9,4 +9,17 @@ class Gui:
         self._window = Tk()
         self._window.title(self.title_text)
         self._window.geometry("500x300")
+
+        self.p_label = Label(text="Мощность")
+        self.p_label.grid(columnspan=1, rowspan=2, column=0, row=0)
+
+        self.u_label = Label(text="Напряжение питания")
+        self.u_label.grid(columnspan=1, rowspan=2, column=0, row=2)
+
+        self.cos_label = Label(text="Характеристика нагрузки cos(fi)")
+        self.cos_label.grid(columnspan=1, rowspan=2, column=0, row=5)
+
+        self.length_label = Label(text="Длина кабеля")
+        self.length_label.grid(columnspan=1, rowspan=2, column=0, row=8)
+
         self._window.mainloop()
