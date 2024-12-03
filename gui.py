314b4e3bcef5e19938f3c2c6f15af2aa1f30f017
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-03 16:53:39 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -8,6 +8,12 @@ class Gui:
     def u_radiobutton_command(self):
         pass
 
+    def cos_radiobutton_command(self):
+        pass
+
+    def length_radiobutton_command(self):
+        pass
+
     def __init__(self):
         self._window = Tk()
         self._window.title(self.title_text)
@@ -35,9 +41,31 @@ class Gui:
         # Исходные данные по характеру нагрузки
         self.cos_label = Label(text="Характеристика нагрузки, cos(fi)")
         self.cos_label.grid(columnspan=2, rowspan=1, column=0, row=5)
+        self.cos_radiobutton_position = IntVar(value=0)
+        self.cos_radiobutton1 = Radiobutton(
+            text="Реактивная",
+            variable=self.cos_radiobutton_position,
+            value=0, command=self.cos_radiobutton_command)
+        self.cos_radiobutton2 = Radiobutton(
+            text="Активная",
+            variable=self.cos_radiobutton_position,
+            value=1, command=self.cos_radiobutton_command)
+        self.cos_radiobutton1.grid(column=0, row=6)
+        self.cos_radiobutton2.grid(column=0, row=7)
 
         # Исходные данные по кабелю
         self.length_label = Label(text="Длина кабеля, м")
         self.length_label.grid(columnspan=2, rowspan=1, column=0, row=8)
+        self.length_radiobutton_position = IntVar(value=0)
+        self.length_radiobutton1 = Radiobutton(
+            text="Медный",
+            variable=self.length_radiobutton_position,
+            value=0, command=self.length_radiobutton_command)
+        self.length_radiobutton2 = Radiobutton(
+            text="Алюминиевый",
+            variable=self.length_radiobutton_position,
+            value=1, command=self.length_radiobutton_command)
+        self.length_radiobutton1.grid(column=0, row=9)
+        self.length_radiobutton2.grid(column=0, row=10)
 
         self._window.mainloop()
