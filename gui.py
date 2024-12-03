c27b2b1361ee8848ca425b8f3731fb30aca6c430
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-03 16:22:56 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
+from tkinter import Tk
 
 
 class Gui:
 
+    title_text = "wire_cacl 0.0"
+
     def __init__(self):
-        pass
-    
+        self._window = Tk()
+        self._window.title(self.title_text)
+        self._window.geometry("500x300")
+        self._window.mainloop()
