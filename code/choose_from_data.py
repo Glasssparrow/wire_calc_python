e101f2342d26e9ad066c378fb0e955090dc53ab9
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-04 18:10:25 +0300

    Update choose_from_data.py

--- code/choose_from_data.py
+++ code/choose_from_data.py
@@ -1,4 +1,15 @@
+from data import *
 
 
 def get_beaker_current(load_current):
-    pass
+    breaker_current = 0
+    for breaker in current_breakers_low_voltage:
+        if breaker > load_current:
+            breaker_current = breaker
+            break
+    if breaker_current != 0:
+        return breaker_current
+    else:
+        raise Exception(
+            f"Не найден подходящий автомат."
+        )
