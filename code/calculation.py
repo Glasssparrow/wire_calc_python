d4a640eb69cde54307c6c17365c6f685da7c1bfa
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-12-04 18:03:06 +0300

    Update calculation.py

--- code/calculation.py
+++ code/calculation.py
@@ -13,6 +13,18 @@ def calculate_load_current(
         raise Exception(f"Unexpected incident")
     return load_current
 
+def calculate_breaker_current(
+        P, U, cos_fi, L,
+        phases, load_type,
+        cable_type,
+):
+    load_current = calculate_load_current(
+        P, U, cos_fi, L,
+        phases, load_type,
+        cable_type,
+    )
+    return "breaker_current"
+
 def calculate_max_cable_current(
         P, U, cos_fi, L,
         phases, load_type,
@@ -26,13 +38,6 @@ def calculate_cable_area(
         cable_type,
 ):
     return "cable_area"
-
-def calculate_breaker_current(
-        P, U, cos_fi, L,
-        phases, load_type,
-        cable_type,
-):
-    return "breaker_current"
 
 def calculate_load_u(
         P, U, cos_fi, L,
